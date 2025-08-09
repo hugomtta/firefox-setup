0122f81ba2311c1350497499871010b35080d13c
Hugo Marotta <hugomtta@proton.me>
2025-08-08 22:10:23 -0300

    Check architecture compatibility

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ set -eo pipefail
 
 # Check OS compatibility
 OS="$(uname -s)"
+ARCH="$(uname -m)"
 
-if [[ "${OS}" != 'Linux' ]]; then
-  echo 'This script is intended for Linux systems only.'
+if [[ "${OS}" != 'Linux' || "${ARCH}" != 'x86_64' ]]; then
+  echo 'This script is intended for Linux 64-bit systems only.'
   exit 1
 fi
