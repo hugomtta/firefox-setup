d2e0ac55ee770b7139b05b5a6fb3f9ca2345d541
Hugo Marotta <hugomtta@proton.me>
2025-08-08 21:27:52 -0300

    Check OS compatibility

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 set -eo pipefail
+
+# Check OS compatibility
+OS="$(uname -s)"
+
+if [[ "${OS}" != 'Linux' ]]; then
+  echo 'This script is intended for Linux systems only.'
+  exit 1
+fi
