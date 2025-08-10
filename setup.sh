b193bd5054042f7c698e397d1fd3cd7df980a122
Hugo Marotta <hugomtta@proton.me>
2025-08-09 21:36:31 -0300

    Revert "Create temporary directory"
    This reverts commit 27f5d616d45e7f4ad3a2b9e140ca2dbbac0f9b9d.
    
    The Firefox tarball is downloaded and extracted directly to the
    installation directory, eliminating the need for an intermediate
    temporary directory.

--- setup.sh
+++ setup.sh
@@ -17,10 +17,6 @@ if [[ "${OS}" != 'Linux' || "${ARCH}" != 'x86_64' ]]; then
   exit 1
 fi
 
-# Create temporary directory
-TEMP_DIR="$(mktemp -d)"
-trap 'rm -rf "${TEMP_DIR}"' EXIT
-
 # Set Firefox language
 FIREFOX_LANG='en-US'
 
